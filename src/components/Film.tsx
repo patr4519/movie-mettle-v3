7557fe0ad2b327275d5462544736c7bea64c12fc
patr4519 <jero96@mail.ru>
2023-07-12 21:53:13 +0300

    +

--- src/components/Film.tsx
+++ src/components/Film.tsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import { useGetFilmQuery } from "../features/api/apiSlice";
 import { useParams } from "react-router-dom";
-
 interface FilmProp {
   title: string;
 }
@@ -15,32 +14,25 @@ export const Film = ({ title }: FilmProp) => {
 
   if (error) return <div>Error while fetch film</div>;
   if (isLoading) return <div>Loading...</div>;
-  if (!data.Title) return <div>{`${title}: not found `}</div>;
+  if (!data?.Title) return <div>{`${title}: not found `}</div>;
 
   return (
     <div className="fav-item">
-      {data.Title && (
-        <div className="content">
-          <Link to={`/movie/${data.Title}`} className="poster-link">
-            <img
-              className="poster"
-              src={data.Poster}
-              alt="poster"
-              width={100}
-            />
-          </Link>
-          <div className="details">
-            <div className="title">
-              <span>{data.Title}</span>, {data.Year}
-            </div>
-            <div className="genre&runtime">
-              {data.Genre}, {data.Runtime}
-            </div>
-            <div className="country">{data.Country}</div>
-            <div className="director">{data.Director}</div>
+      <div className="content">
+        <Link to={`/movie/${data.Title}`} className="poster-link">
+          <img className="poster" src={data.Poster} alt="poster" width={100} />
+        </Link>
+        <div className="details">
+          <div className="title">
+            <span>{data.Title}</span>, {data.Year}
           </div>
+          <div className="genre&runtime">
+            {data.Genre}, {data.Runtime}
+          </div>
+          <div className="country">{data.Country}</div>
+          <div className="director">{data.Director}</div>
         </div>
-      )}
+      </div>
     </div>
   );
 };
--- src/features/api/apiSlice.tsx
+++ src/features/api/apiSlice.tsx
@@ -1,6 +1,37 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // http://www.omdbapi.com/?t=The Mummy&apikey=9fc0fef8
+export interface FilmResponse {
+  Title: string;
+  Year: string;
+  Rated: string;
+  Released: string;
+  Runtime: string;
+  Genre: string;
+  Director: string;
+  Writer: string;
+  Actors: string;
+  Plot: string;
+  Language: string;
+  Country: string;
+  Awards: string;
+  Poster: string;
+  Ratings: FilmRating[],
+  Metascore: string;
+  imdbRating: string;
+  imdbVotes: string;
+  imdbID: string;
+  Type: string;
+  DVD: string;
+  BoxOffice: string;
+  Production: string;
+  Website: string;
+  Response: string;
+}
+interface FilmRating {
+  Source: string;
+  Value: string;
+}
 
 export const apiSlice = createApi({
   reducerPath: "api",
@@ -8,9 +39,9 @@ export const apiSlice = createApi({
     baseUrl: "http://www.omdbapi.com",
   }),
   endpoints: (builder) => ({
-    getFilm: builder.query({
+    getFilm: builder.query<FilmResponse, string>({
       query: (title) => `/?t=${title}&apikey=9fc0fef8`,
-      keepUnusedDataFor: 300
+      keepUnusedDataFor: 300,
     }),
   }),
 });
